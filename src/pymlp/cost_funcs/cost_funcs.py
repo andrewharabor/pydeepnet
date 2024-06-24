63ee897d86e35e6842605efb6405b4b3aa59c777
Andrew Harabor <andrew.harabor@gmail.com>
2024-06-24 10:58:17 -0700

    Rename parameters for consistency

--- src/pymlp/cost_funcs/cost_funcs.py
+++ src/pymlp/cost_funcs/cost_funcs.py
@@ -4,33 +4,33 @@ from pymlp.typing import *
 
 class CostFunc(ABC):
     @abstractmethod
-    def compute(self, predicted: NDArray, expected: NDArray) -> Float64:
+    def compute(self, predictions: NDArray, targets: NDArray) -> Float64:
         pass
 
     @abstractmethod
-    def derivative(self, predicted: NDArray, expected: NDArray) -> NDArray:
+    def derivative(self, predictions: NDArray, targets: NDArray) -> NDArray:
         pass
 
     @staticmethod
-    def _assert_shapes(predicted: NDArray, expected: NDArray) -> None:
-        if predicted.shape[0] == 0:
-            raise ValueError("`predicted` array is empty")
-        if expected.shape[0] == 0:
-            raise ValueError("`expected` array is empty")
-        if predicted.shape != expected.shape:
-            raise ValueError("`predicted` and `expected` arrays have different shapes")
+    def _assert_shapes(predictions: NDArray, targets: NDArray) -> None:
+        if predictions.shape[0] == 0:
+            raise ValueError("`predictions` array is empty")
+        if targets.shape[0] == 0:
+            raise ValueError("`targets` array is empty")
+        if predictions.shape != targets.shape:
+            raise ValueError("`predictions` and `targets` arrays have different shapes")
 
 
 class CrossEntropy(CostFunc):
-    offset: Float64 = Float64(1e-9)
+    offset: Float64 = Float64(1e-9)  # to avoid logarithm of or division by zero
 
-    def compute(self, predicted: NDArray, expected: NDArray) -> Float64:
-        CrossEntropy._assert_shapes(predicted, expected)
-        return -np.sum(expected * np.log(predicted + self.offset))
+    def compute(self, predictions: NDArray, targets: NDArray) -> Float64:
+        CrossEntropy._assert_shapes(predictions, targets)
+        return -np.sum(targets * np.log(predictions + self.offset))
 
-    def derivative(self, predicted: NDArray, expected: NDArray) -> NDArray:
-        CrossEntropy._assert_shapes(predicted, expected)
-        return -expected / (predicted + self.offset)
+    def derivative(self, predictions: NDArray, targets: NDArray) -> NDArray:
+        CrossEntropy._assert_shapes(predictions, targets)
+        return -targets / (predictions + self.offset)
 
 
 class Huber(CostFunc):
@@ -39,42 +39,42 @@ class Huber(CostFunc):
     def __init__(self, threshold: Float64) -> None:
         self.threshold = threshold
 
-    def compute(self, predicted: NDArray, expected: NDArray) -> Float64:
-        Huber._assert_shapes(predicted, expected)
-        diff: NDArray = np.abs(predicted - expected)
-        return np.sum(np.where(diff <= self.threshold, 0.5 * (diff ** 2), self.threshold * (diff - 0.5 * self.threshold))) / predicted.shape[0]
+    def compute(self, predictions: NDArray, targets: NDArray) -> Float64:
+        Huber._assert_shapes(predictions, targets)
+        diff: NDArray = np.abs(predictions - targets)
+        return np.sum(np.where(diff <= self.threshold, 0.5 * (diff ** 2), self.threshold * (diff - 0.5 * self.threshold))) / predictions.shape[0]
 
-    def derivative(self, predicted: NDArray, expected: NDArray) -> NDArray:
-        Huber._assert_shapes(predicted, expected)
-        diff: NDArray = predicted - expected
-        return np.where(np.abs(diff) <= self.threshold, diff, self.threshold * np.sign(diff)) / predicted.shape[0]
+    def derivative(self, predictions: NDArray, targets: NDArray) -> NDArray:
+        Huber._assert_shapes(predictions, targets)
+        diff: NDArray = predictions - targets
+        return np.where(np.abs(diff) <= self.threshold, diff, self.threshold * np.sign(diff)) / predictions.shape[0]
 
 
 class LogCosh(CostFunc):
-    def compute(self, predicted: NDArray, expected: NDArray) -> Float64:
-        LogCosh._assert_shapes(predicted, expected)
-        return np.sum(np.log(np.cosh(predicted - expected))) / predicted.shape[0]
+    def compute(self, predictions: NDArray, targets: NDArray) -> Float64:
+        LogCosh._assert_shapes(predictions, targets)
+        return np.sum(np.log(np.cosh(predictions - targets))) / predictions.shape[0]
 
-    def derivative(self, predicted: NDArray, expected: NDArray) -> NDArray:
-        LogCosh._assert_shapes(predicted, expected)
-        return np.tanh(predicted - expected) / predicted.shape[0]
+    def derivative(self, predictions: NDArray, targets: NDArray) -> NDArray:
+        LogCosh._assert_shapes(predictions, targets)
+        return np.tanh(predictions - targets) / predictions.shape[0]
 
 
 class MeanAbsoluteError(CostFunc):
-    def compute(self, predicted: NDArray, expected: NDArray) -> Float64:
-        MeanAbsoluteError._assert_shapes(predicted, expected)
-        return np.sum(np.abs(predicted - expected)) / predicted.shape[0]
+    def compute(self, predictions: NDArray, targets: NDArray) -> Float64:
+        MeanAbsoluteError._assert_shapes(predictions, targets)
+        return np.sum(np.abs(predictions - targets)) / predictions.shape[0]
 
-    def derivative(self, predicted: NDArray, expected: NDArray) -> NDArray:
-        MeanAbsoluteError._assert_shapes(predicted, expected)
-        return np.sign(predicted - expected) / predicted.shape[0]
+    def derivative(self, predictions: NDArray, targets: NDArray) -> NDArray:
+        MeanAbsoluteError._assert_shapes(predictions, targets)
+        return np.sign(predictions - targets) / predictions.shape[0]
 
 
 class MeanSquaredError(CostFunc):
-    def compute(self, predicted: NDArray, expected: NDArray) -> Float64:
-        MeanSquaredError._assert_shapes(predicted, expected)
-        return np.sum((predicted - expected) ** 2) / (2 * predicted.shape[0])
+    def compute(self, predictions: NDArray, targets: NDArray) -> Float64:
+        MeanSquaredError._assert_shapes(predictions, targets)
+        return np.sum((predictions - targets) ** 2) / (2 * predictions.shape[0])
 
-    def derivative(self, predicted: NDArray, expected: NDArray) -> NDArray:
-        MeanSquaredError._assert_shapes(predicted, expected)
-        return (predicted - expected) / predicted.shape[0]
+    def derivative(self, predictions: NDArray, targets: NDArray) -> NDArray:
+        MeanSquaredError._assert_shapes(predictions, targets)
+        return (predictions - targets) / predictions.shape[0]
