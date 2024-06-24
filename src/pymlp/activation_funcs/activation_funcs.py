7721b9278eb5d4a24129d107aa8bc8ad90c01f2a
Andrew Harabor <andrew.harabor@gmail.com>
2024-06-24 10:53:56 -0700

    Add input validation and reorder alphabetically

--- src/pymlp/activation_funcs/activation_funcs.py
+++ src/pymlp/activation_funcs/activation_funcs.py
@@ -11,6 +11,31 @@ class ActivationFunc(ABC):
     def derivative(self, vector: NDArray) -> NDArray:
         pass
 
+    @staticmethod
+    def _assert_shape(vector: NDArray) -> None:
+        if vector.shape[0] == 0:
+            raise ValueError("`vector` array is empty")
+
+
+class BinaryStep(ActivationFunc):
+    def compute(self, vector: NDArray) -> NDArray:
+        BinaryStep._assert_shape(vector)
+        return np.where(vector >= 0, 1, 0)
+
+    def derivative(self, vector: NDArray) -> NDArray:
+        BinaryStep._assert_shape(vector)
+        return np.diag(np.zeros_like(vector))
+
+
+class Linear(ActivationFunc):
+    def compute(self, vector: NDArray) -> NDArray:
+        Linear._assert_shape(vector)
+        return vector
+
+    def derivative(self, vector: NDArray) -> NDArray:
+        Linear._assert_shape(vector)
+        return np.diag(np.ones_like(vector))
+
 
 class ReLU(ActivationFunc):
     leak: Float64  # for leaky ReLU
@@ -19,67 +44,63 @@ class ReLU(ActivationFunc):
         self.leak = leak
 
     def compute(self, vector: NDArray) -> NDArray:
+        ReLU._assert_shape(vector)
         return np.maximum(vector, self.leak * vector)
 
     def derivative(self, vector: NDArray) -> NDArray:
+        ReLU._assert_shape(vector)
         return np.diag(np.where(vector > 0, 1, self.leak))
-
-
-class Softmax(ActivationFunc):
-    def compute(self, vector: NDArray) -> NDArray:
-        exp: NDArray = np.exp(vector - np.max(vector))
-        return exp / np.sum(exp)
-
-    def derivative(self, vector: NDArray) -> NDArray:
-        softmax: NDArray = self.compute(vector)
-        return np.diag(softmax) - np.outer(softmax, softmax)
 
 
 class Sigmoid(ActivationFunc):
     def compute(self, vector: NDArray) -> NDArray:
+        Sigmoid._assert_shape(vector)
         return 1 / (1 + np.exp(-vector))
 
     def derivative(self, vector: NDArray) -> NDArray:
+        Sigmoid._assert_shape(vector)
         sigmoid: NDArray = self.compute(vector)
         return np.diag(sigmoid * (1 - sigmoid))
 
 
-class Tanh(ActivationFunc):
-    def compute(self, vector: NDArray) -> NDArray:
-        return np.tanh(vector)
-
-    def derivative(self, vector: NDArray) -> NDArray:
-        return np.diag(1 - np.tanh(vector) ** 2)
-
-
 class SiLU(ActivationFunc):
     def compute(self, vector: NDArray) -> NDArray:
+        SiLU._assert_shape(vector)
         return vector / (1 + np.exp(-vector))
 
     def derivative(self, vector: NDArray) -> NDArray:
+        SiLU._assert_shape(vector)
         sigmoid: NDArray = 1 / (1 + np.exp(-vector))
         return np.diag(sigmoid + (vector * sigmoid * (1 - sigmoid)))
 
 
+class Softmax(ActivationFunc):
+    def compute(self, vector: NDArray) -> NDArray:
+        Softmax._assert_shape(vector)
+        exp: NDArray = np.exp(vector - np.max(vector))
+        return exp / np.sum(exp)
+
+    def derivative(self, vector: NDArray) -> NDArray:
+        Softmax._assert_shape(vector)
+        softmax: NDArray = self.compute(vector)
+        return np.diag(softmax) - np.outer(softmax, softmax)
+
+
 class Softplus(ActivationFunc):
     def compute(self, vector: NDArray) -> NDArray:
+        Softplus._assert_shape(vector)
         return np.log(1 + np.exp(vector))
 
     def derivative(self, vector: NDArray) -> NDArray:
+        Softplus._assert_shape(vector)
         return np.diag(1 / (1 + np.exp(-vector)))
 
 
-class BinaryStep(ActivationFunc):
+class Tanh(ActivationFunc):
     def compute(self, vector: NDArray) -> NDArray:
-        return np.where(vector >= 0, 1, 0)
+        Tanh._assert_shape(vector)
+        return np.tanh(vector)
 
     def derivative(self, vector: NDArray) -> NDArray:
-        return np.diag(np.zeros_like(vector))
-
-
-class Linear(ActivationFunc):
-    def compute(self, vector: NDArray) -> NDArray:
-        return vector
-
-    def derivative(self, vector: NDArray) -> NDArray:
-        return np.diag(np.ones_like(vector))
+        Tanh._assert_shape(vector)
+        return np.diag(1 - np.tanh(vector) ** 2)
--- src/pymlp/cost_funcs/cost_funcs.py
+++ src/pymlp/cost_funcs/cost_funcs.py
@@ -11,39 +11,26 @@ class CostFunc(ABC):
     def derivative(self, predicted: NDArray, expected: NDArray) -> NDArray:
         pass
 
-
-class MeanSquaredError(CostFunc):
-    def compute(self, predicted: NDArray, expected: NDArray) -> Float64:
-        return np.sum((predicted - expected) ** 2) / (2 * predicted.shape[0])
-
-    def derivative(self, predicted: NDArray, expected: NDArray) -> NDArray:
-        return (predicted - expected) / predicted.shape[0]
+    @staticmethod
+    def _assert_shapes(predicted: NDArray, expected: NDArray) -> None:
+        if predicted.shape[0] == 0:
+            raise ValueError("`predicted` array is empty")
+        if expected.shape[0] == 0:
+            raise ValueError("`expected` array is empty")
+        if predicted.shape != expected.shape:
+            raise ValueError("`predicted` and `expected` arrays have different shapes")
 
 
 class CrossEntropy(CostFunc):
-    offset: Float64 = Float64(1e-9)  # to avoid logarithm of or division by zero
+    offset: Float64 = Float64(1e-9)
 
     def compute(self, predicted: NDArray, expected: NDArray) -> Float64:
+        CrossEntropy._assert_shapes(predicted, expected)
         return -np.sum(expected * np.log(predicted + self.offset))
 
     def derivative(self, predicted: NDArray, expected: NDArray) -> NDArray:
+        CrossEntropy._assert_shapes(predicted, expected)
         return -expected / (predicted + self.offset)
-
-
-class MeanAbsoluteError(CostFunc):
-    def compute(self, predicted: NDArray, expected: NDArray) -> Float64:
-        return np.sum(np.abs(predicted - expected)) / predicted.shape[0]
-
-    def derivative(self, predicted: NDArray, expected: NDArray) -> NDArray:
-        return np.sign(predicted - expected) / predicted.shape[0]
-
-
-class LogCosh(CostFunc):
-    def compute(self, predicted: NDArray, expected: NDArray) -> Float64:
-        return np.sum(np.log(np.cosh(predicted - expected))) / predicted.shape[0]
-
-    def derivative(self, predicted: NDArray, expected: NDArray) -> NDArray:
-        return np.tanh(predicted - expected) / predicted.shape[0]
 
 
 class Huber(CostFunc):
@@ -53,9 +40,41 @@ class Huber(CostFunc):
         self.threshold = threshold
 
     def compute(self, predicted: NDArray, expected: NDArray) -> Float64:
+        Huber._assert_shapes(predicted, expected)
         diff: NDArray = np.abs(predicted - expected)
         return np.sum(np.where(diff <= self.threshold, 0.5 * (diff ** 2), self.threshold * (diff - 0.5 * self.threshold))) / predicted.shape[0]
 
     def derivative(self, predicted: NDArray, expected: NDArray) -> NDArray:
+        Huber._assert_shapes(predicted, expected)
         diff: NDArray = predicted - expected
         return np.where(np.abs(diff) <= self.threshold, diff, self.threshold * np.sign(diff)) / predicted.shape[0]
+
+
+class LogCosh(CostFunc):
+    def compute(self, predicted: NDArray, expected: NDArray) -> Float64:
+        LogCosh._assert_shapes(predicted, expected)
+        return np.sum(np.log(np.cosh(predicted - expected))) / predicted.shape[0]
+
+    def derivative(self, predicted: NDArray, expected: NDArray) -> NDArray:
+        LogCosh._assert_shapes(predicted, expected)
+        return np.tanh(predicted - expected) / predicted.shape[0]
+
+
+class MeanAbsoluteError(CostFunc):
+    def compute(self, predicted: NDArray, expected: NDArray) -> Float64:
+        MeanAbsoluteError._assert_shapes(predicted, expected)
+        return np.sum(np.abs(predicted - expected)) / predicted.shape[0]
+
+    def derivative(self, predicted: NDArray, expected: NDArray) -> NDArray:
+        MeanAbsoluteError._assert_shapes(predicted, expected)
+        return np.sign(predicted - expected) / predicted.shape[0]
+
+
+class MeanSquaredError(CostFunc):
+    def compute(self, predicted: NDArray, expected: NDArray) -> Float64:
+        MeanSquaredError._assert_shapes(predicted, expected)
+        return np.sum((predicted - expected) ** 2) / (2 * predicted.shape[0])
+
+    def derivative(self, predicted: NDArray, expected: NDArray) -> NDArray:
+        MeanSquaredError._assert_shapes(predicted, expected)
+        return (predicted - expected) / predicted.shape[0]
