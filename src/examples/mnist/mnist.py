e23a1092b1cf0ae17870152f69e038acf66242f9
Andrew Harabor <andrew.harabor@gmail.com>
2024-06-27 17:21:00 -0700

    Add MNIST example

---
+++ src/examples/mnist/mnist.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+import numpy as np
+
+from pymlp import NeuralNetwork
+from pymlp.activation_funcs import ReLU, Softmax
+from pymlp.cost_funcs import CrossEntropy
+from pymlp.error_metrics import PercentCorrect
+from pymlp.layers import DenseLayer, DenseOutputLayer, InputLayer
+from pymlp.normalizers import ZScore
+from pymlp.optimizers import Adam
+from pymlp.regularizers import ElasticNet
+from pymlp.typing import Float64, Int64, NDArray
+
+# Hyperparameters
+HIDDEN_LAYER_SIZE: Int64 = Int64(200)
+RELU_LEAK: Float64 = Float64(0.01)
+REG_PENALTY: Float64 = Float64(5e-4)
+LEARNING_RATE: Float64 = Float64(0.02)
+DECAY_RATE1: Float64 = Float64(0.9)
+DECAY_RATE2: Float64 = Float64(0.999)
+EPOCHS: Int64 = Int64(15)
+BATCH_SIZE: Int64 = Int64(32)
+
+# Load MNIST data
+train_inputs: NDArray = np.load("src/examples/mnist/data/train_inputs.npy")
+train_targets: NDArray = np.load("src/examples/mnist/data/train_targets.npy")
+test_inputs: NDArray = np.load("src/examples/mnist/data/test_inputs.npy")
+test_targets: NDArray = np.load("src/examples/mnist/data/test_targets.npy")
+
+# Create, train, and evaluate neural network
+network: NeuralNetwork = NeuralNetwork(
+    InputLayer(Int64(train_inputs.shape[1]), ZScore()),
+    [
+        DenseLayer(HIDDEN_LAYER_SIZE, ReLU(RELU_LEAK), ElasticNet(REG_PENALTY, REG_PENALTY)),
+    ],
+    DenseOutputLayer(Int64(train_targets.shape[1]), Softmax(), ElasticNet(REG_PENALTY, REG_PENALTY), CrossEntropy()),
+    Adam(LEARNING_RATE, DECAY_RATE1, DECAY_RATE2),
+    PercentCorrect()
+)
+
+network.train(train_inputs, train_targets, EPOCHS, BATCH_SIZE)
+
+# hidden_layer_weights: NDArray = np.load("src/examples/mnist/parameters/hidden_layer_weights.npy")
+# hidden_layer_biases: NDArray = np.load("src/examples/mnist/parameters/hidden_layer_biases.npy")
+# output_layer_weights: NDArray = np.load("src/examples/mnist/parameters/output_layer_weights.npy")
+# output_layer_biases: NDArray = np.load("src/examples/mnist/parameters/output_layer_biases.npy")
+# network.load_parameters([(hidden_layer_weights, hidden_layer_biases)], (output_layer_weights, output_layer_biases))
+# if network.input_layer.normalizer is not None:
+#     network.input_layer.normalizer.adapt(train_inputs)  # normalizer is only adapted to input data in `train()` method
+
+train_predictions: NDArray = network.predict(train_inputs)
+test_predictions: NDArray = network.predict(test_inputs)
+print(f"Train Data Accuracy: {(100 * network.evaluate(train_targets, train_predictions)):.2f}%")
+print(f"Test Data Accuracy: {(100 * network.evaluate(test_targets, test_predictions)):.2f}%")
