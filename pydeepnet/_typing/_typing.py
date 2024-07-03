06df06d511030f1fd78d298c1c40b436051529e5
Andrew Harabor <andrew.harabor@gmail.com>
2024-07-03 08:36:19 -0700

    Add `pytest` to `_typing` module

--- pydeepnet/_typing/_typing.py
+++ pydeepnet/_typing/_typing.py
@@ -6,6 +6,7 @@ from typing import Any, TypeAlias
 import numpy as np
 import numpy.linalg as npla
 import numpy.typing as npt
+import pytest
 
 Int64: TypeAlias = np.int64
 Float64: TypeAlias = np.float64
