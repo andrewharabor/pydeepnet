3f7caa7e506b22bdb27262e54e549f72742d1d49
Andrew Harabor <andrew.harabor@gmail.com>
2024-07-05 22:39:18 -0700

    Remove tests for `optimizers` and `layers` modules

--- pydeepnet/layers/__init__.py
+++ pydeepnet/layers/__init__.py
@@ -1,6 +1,1 @@
-from pydeepnet._pytest_tester import PytestTester
-
-run_tests = PytestTester(__name__).run
-del PytestTester
-
 from .layers import DenseLayer, DenseOutputLayer, InputLayer, Layer
--- pydeepnet/optimizers/__init__.py
+++ pydeepnet/optimizers/__init__.py
@@ -1,6 +1,1 @@
-from pydeepnet._pytest_tester import PytestTester
-
-run_tests = PytestTester(__name__).run
-del PytestTester
-
 from .optimizers import Adam, GradientDescent, Momentum, Optimizer, RMSProp
